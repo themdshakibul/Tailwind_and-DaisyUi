9d3d0b445fe32e5f896887ff133471ad738b0ef6
themdshakibul <mdshakibul88240@gmail.com>
2026-03-13 21:56:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./tailwind-init.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-4xl font-bold">
+        <h2>Hello Iam Md Shakibul Islam</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
